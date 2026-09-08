87ccb5c0393eed076696000bc183b96bc1a2891c
zekethecutie <2.21992169e+08+zekethecutie@users.noreply.github.com>
2026-09-08 08:25:12 +0000

    feat: greet verified members in community chat

--- src/state.js
+++ src/state.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
-const emptyGuild = () => ({ channels: {}, categories: {}, roles: {}, messages: {}, config: {}, members: {}, changelog: [] });
+const emptyGuild = () => ({ channels: {}, categories: {}, roles: {}, messages: {}, config: {}, members: {}, changelog: [], welcome: { lastAt: 0, recentMemberIds: [] } });
 
 export async function openState(dataDir) {
   await fs.mkdir(dataDir, { recursive: true });
